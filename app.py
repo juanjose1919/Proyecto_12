098f5b6f2601a88a362ecdfee934e1c1b2a349ad
juanjose1919 <83190654+juanjose1919@users.noreply.github.com>
2022-11-12 12:51:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-
+import pandas as pd
 st.title('INDICE DE LA FELICIDAD')
-st.header('Aquí se observan 10 variables ontemidas del Banco Mundial cuyo proposito es de expicar la felicidad de un país, cuya muestra es de 60 países alrededor del mundo')
+st.subheader('Aquí se observan 10 variables ontemidas del Banco Mundial cuyo proposito es de expicar la felicidad de un país, cuya muestra es de 60 países alrededor del mundo')
 
-
+data = pd.read_csv('/carpeta/datasets3.csv')
 
