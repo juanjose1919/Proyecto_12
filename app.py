5db4368d8e365d4d472eb46e9b1dae6c38a7c050
juanjose1919 <83190654+juanjose1919@users.noreply.github.com>
2022-11-12 12:45:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title('INDICE DE LA FELICIDAD')
-st.hearder('Aquí se observan 10 variables ontemidas del Banco Mundial cuyo proposito es de expicar la felicidad de un país, cuya muestra es de 60 países alrededor del mundo')
+st.header('Aquí se observan 10 variables ontemidas del Banco Mundial cuyo proposito es de expicar la felicidad de un país, cuya muestra es de 60 países alrededor del mundo')
 
 
 
