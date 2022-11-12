a2befdcbaea7479f7cf8fc6f6c70eba46644931a
juanjose1919 <83190654+juanjose1919@users.noreply.github.com>
2022-11-12 12:54:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import pandas as pd
 st.title('INDICE DE LA FELICIDAD')
 st.subheader('Aquí se observan 10 variables ontemidas del Banco Mundial cuyo proposito es de expicar la felicidad de un país, cuya muestra es de 60 países alrededor del mundo')
 
-data = pd.read_csv('/carpeta/datasets3.csv')
-
+data = pd.read_csv('./carpeta/data.csv')
+st.table(data.head(10))
