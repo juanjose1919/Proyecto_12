c18df063200306bbe5738f4da8f1bf7d52608898
juanjose1919 <83190654+juanjose1919@users.noreply.github.com>
2022-11-12 13:15:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,14 +13,11 @@ st.table(data.describe())
 st.subheader('El algoritmo de reducción de dimensionalidad seleccionado es:')
 
 st.subheader('PCA:')
-st.text('Recordemos que es un método estadístico el cual permite simplificar la complejidad de espacios muestrales con muchas dimensiones (10 dimensiones) a la vez que conserva su información')
+st.write('Recordemos que es un método estadístico el cual permite simplificar la complejidad de espacios muestrales con muchas dimensiones (10 dimensiones) a la vez que conserva su información')
              
-
-st.table(
-'Estandarizar el dataset'
-'Construir la matriz de covarianzas'
-'Descomponer la matriz de covarianzas en valores propios y vectores propios'
-'Seleccionar  vectores propios, los cuales corresponden a los  vectores más grandes, donde  representa la dimensionalidad del nuevo dataset'
-'Construir una matriz de proyección , del top de  vectores propios'
-'Transformar el dataset input, , utilizando la matriz de proyección  para obtener el nuevo subespacio -dimensional'
-)
+st.write('1. Estandarizar el dataset')
+st.write('2. Construir la matriz de covarianzas')
+st.write('3. Descomponer la matriz de covarianzas en valores propios y vectores propios')
+st.write('4. Seleccionar  vectores propios, los cuales corresponden a los  vectores más grandes, donde  representa la dimensionalidad del nuevo dataset')
+st.write('5. Construir una matriz de proyección , del top de  vectores propios')
+st.write('6. Transformar el dataset input, , utilizando la matriz de proyección  para obtener el nuevo subespacio -dimensional')
