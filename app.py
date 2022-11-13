d473d91c785dabbdcef0b60306a15b19ebb1558f
ValeriaMendezC <110490590+ValeriaMendezC@users.noreply.github.com>
2022-11-12 22:53:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import plotly.graph_objs as go
 
 st.title('INDICE DE LA FELICIDAD')
-st.subheader('Aquí se observan 10 variables ontemidas del Banco Mundial cuyo proposito es de expicar la felicidad de un país, cuya muestra es de 60 países alrededor del mundo')
+st.subheader('Aquí se observan 10 variables obtenidas del Banco Mundial cuyo proposito es de expicar la felicidad de un país, cuya muestra es de 60 países alrededor del mundo')
 
 data = pd.read_csv('./carpeta/data.csv')
 st.header('Los primeros diez países y las variables analizadas:')
